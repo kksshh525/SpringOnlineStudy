072e76154fffb757e3975065b30d97c80b9cbeb6
kksshh525 <kksshh525@naver.com>
2019-05-06 00:31:34 +0900

    Add Env lecture

---
+++ Environment/src/main/java/com/javalec/ex/SubAdminConnection.java
@@ -0,0 +1,22 @@
+package com.javalec.ex;
+
+public class SubAdminConnection {
+    private String adminId;
+    private String adminPw;
+
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminPw() {
+        return adminPw;
+    }
+
+    public void setAdminPw(String adminPw) {
+        this.adminPw = adminPw;
+    }
+}
